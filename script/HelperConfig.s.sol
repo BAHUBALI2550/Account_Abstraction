a6b6276f0ec13f982ad55e8bbb8c8d514b3f57d4
Bahubali2550 <hs49271.rkt@gmail.com>
2024-09-07 19:44:02 +0530

    added tests and PackedUser

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console2} from "forge-std/Script.sol";
+import {EntryPoint} from "lib/account-abstraction/contracts/core/EntryPoint.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     error HelperConfig__InvalidChainId();
@@ -28,9 +30,9 @@ contract HelperConfig is Script {
 
     constructor() {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getEthSepoliaConfig();
-        // networkConfigs[ETH_MAINNET_CHAIN_ID] = getEthMainnetConfig();
-        // networkConfigs[ZKSYNC_MAINNET_CHAIN_ID] = getZkSyncConfig();
-        // networkConfigs[ARBITRUM_MAINNET_CHAIN_ID] = getArbMainnetConfig();
+        networkConfigs[ETH_MAINNET_CHAIN_ID] = getEthMainnetConfig();
+        networkConfigs[ZKSYNC_MAINNET_CHAIN_ID] = getZkSyncConfig();
+        networkConfigs[ARBITRUM_MAINNET_CHAIN_ID] = getArbMainnetConfig();
     }
 
     function getConfig() public returns (NetworkConfig memory) {
@@ -50,15 +52,15 @@ contract HelperConfig is Script {
     /*//////////////////////////////////////////////////////////////
                                 CONFIGS
     //////////////////////////////////////////////////////////////*/
-    // function getEthMainnetConfig() public pure returns (NetworkConfig memory) {
-    //     // This is v7
-    //     return NetworkConfig({
-    //         entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
-    //         usdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
-    //         account: BURNER_WALLET
-    //     });
-    //     // https://blockscan.com/address/0x0000000071727De22E5E9d8BAf0edAc6f37da032
-    // }
+    function getEthMainnetConfig() public pure returns (NetworkConfig memory) {
+        // This is v7
+        return NetworkConfig({
+            entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+            // usdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+            account: BURNER_WALLET
+        });
+        // https://blockscan.com/address/0x0000000071727De22E5E9d8BAf0edAc6f37da032
+    }
 
     function getEthSepoliaConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
@@ -68,13 +70,13 @@ contract HelperConfig is Script {
         });
     }
 
-    // function getArbMainnetConfig() public pure returns (NetworkConfig memory) {
-    //     return NetworkConfig({
-    //         entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
-    //         usdc: 0xaf88d065e77c8cC2239327C5EDb3A432268e5831,
-    //         account: BURNER_WALLET
-    //     });
-    // }
+    function getArbMainnetConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: 0x0000000071727De22E5E9d8BAf0edAc6f37da032,
+            // usdc: 0xaf88d065e77c8cC2239327C5EDb3A432268e5831,
+            account: BURNER_WALLET
+        });
+    }
 
     function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
         return NetworkConfig({
@@ -83,29 +85,31 @@ contract HelperConfig is Script {
             account: BURNER_WALLET
         });
     }
-    // function getZkSyncConfig() public pure returns (NetworkConfig memory) {
-    //     return NetworkConfig({
-    //         entryPoint: address(0), // supports native AA, so no entry point needed
-    //         usdc: 0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4,
-    //         account: BURNER_WALLET
-    //     });
-    // }
+    function getZkSyncConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entryPoint: address(0), // supports native AA, so no entry point needed
+            // usdc: 0x1d17CBcF0D6D143135aE902365D2E5e2A16538D4,
+            account: BURNER_WALLET
+        });
+    }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         if (localNetworkConfig.account != address(0)) {
             return localNetworkConfig;
         }
 
-        // // deploy mocks
-        // console2.log("Deploying mocks...");
-        // vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
-        // EntryPoint entryPoint = new EntryPoint();
-        // ERC20Mock erc20Mock = new ERC20Mock();
-        // vm.stopBroadcast();
-        // console2.log("Mocks deployed!");
+        // deploy mocks
+        console2.log("Deploying mocks...");
+        vm.startBroadcast(ANVIL_DEFAULT_ACCOUNT);
+        EntryPoint entryPoint = new EntryPoint();
+        ERC20Mock erc20Mock = new ERC20Mock();
+        vm.stopBroadcast();
+        console2.log("Mocks deployed!");
 
-        // localNetworkConfig =
-        //     NetworkConfig({entryPoint: address(entryPoint), usdc: address(erc20Mock), account: ANVIL_DEFAULT_ACCOUNT});
-        // return localNetworkConfig;
+        localNetworkConfig =
+            NetworkConfig({entryPoint: address(entryPoint), 
+            // usdc: address(erc20Mock), 
+            account: ANVIL_DEFAULT_ACCOUNT});
+        return localNetworkConfig;
     }
 }
